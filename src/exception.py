ec34ce942b55a1546630de478cf1d607735993f1
kalyanram2201 <kalyanramakula13@gmail.com>
2025-09-13 23:44:35 +0530

    new Updates

--- src/exception.py
+++ src/exception.py
@@ -1,6 +1,6 @@
 import sys
 import logging
-
+from src.logger import logging
 def error_message_detail(error, error_detail: sys):
     _, _, exc_tb = error_detail.exc_info()  # traceback object
     file_name = exc_tb.tb_frame.f_code.co_filename
@@ -19,3 +19,9 @@ class CustomException(Exception):
         return self.error_message
 
 
+if __name__=="__main__":
+    try:
+        a=1/0
+    except Exception as a:
+        logging.info("Divide by Zero")
+        raise CustomException(a,sys)
